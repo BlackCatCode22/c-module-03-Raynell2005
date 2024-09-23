7238a7b6c08c2d4d4c64a5124d2f93527d71200c
Raynell2005 <littlefieldraynell@gmail.com>
2024-09-23 12:07:04 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    // 1. Create a new node
+    // 2. Fill data fields
+    // 3. Insert at head of list
+    // 4. Reposition pHead
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
